bf500ddfb2117bb390521dc02e19a561cfb09710
ktc <ktc@statsbiblioteket.dk>
2014-02-04 10:07:31 +0100

    reneame and update shell script for staring the component

--- src/main/scripts/pollAndWork.sh
+++ src/main/scripts/pollAndWork.sh
@@ -9,5 +9,5 @@
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 java -classpath "$SCRIPT_DIR/../conf:$SCRIPT_DIR/../lib/*" \
- dk.statsbiblioteket.newspaper.promptdomsingester.component.PromptDomsIngesterComponent \
+ dk.statsbiblioteket.newspaper.promptdomsingester.component.DomsEnricherComponent \
  $SCRIPT_DIR/../conf/config.properties
