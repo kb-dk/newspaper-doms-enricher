18022673f199bdb3b6be7e0f2460a0682d21361e
jrgatsb <jrg@statsbiblioteket.dk>
2014-02-20 17:51:05 +0100

    More documentation

--- src/main/java/dk/statsbiblioteket/newspaper/domsenricher/component/util/RdfManipulator.java
+++ src/main/java/dk/statsbiblioteket/newspaper/domsenricher/component/util/RdfManipulator.java
@@ -10,18 +10,16 @@ import javax.xml.transform.TransformerException;
  *
  */
 public class RdfManipulator {
-
     Document document;
     Node rdfDescriptionNode;
 
-    public static final String MODEL_TEMPLATE =  "<hasModel xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" " +
-            "xmlns=\"info:fedora/fedora-system:def/model#\" " +
-            "rdf:resource=\"info:fedora/PID\"/>";
+    public static final String MODEL_TEMPLATE =  "<hasModel xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" "
+            + "xmlns=\"info:fedora/fedora-system:def/model#\" "
+            + "rdf:resource=\"info:fedora/PID\"/>";
 
-    public static final String EXTERNAL_RELATION_TEMPLATE = "<NAME xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" " +
-            "xmlns=\"info:fedora/fedora-system:def/relations-external#\" " +
-            "rdf:resource=\"info:fedora/PID\"/>";
-
+    public static final String EXTERNAL_RELATION_TEMPLATE = "<NAME xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" "
+            + "xmlns=\"info:fedora/fedora-system:def/relations-external#\" "
+            + "rdf:resource=\"info:fedora/PID\"/>";
 
     public static final XPathSelector X_PATH_SELECTOR = DOM.createXPathSelector(
             "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
@@ -44,18 +42,22 @@ public class RdfManipulator {
     }
 
     /**
+     * Incorporates the given fragment of rdf/xml as a rdf:description node of the given document
+     * @param fragment The rdf/xml fragment to be added
+     */
+    private void addFragmentToDescription(String fragment) {
+        Document fragmentNode = DOM.stringToDOM(fragment, true);
+        Node importedNode = document.importNode(fragmentNode.getDocumentElement(), true);
+        rdfDescriptionNode.appendChild(importedNode);
+    }
+
+    /**
      * Adds a content model.
      * @param modelPid the full pid of the model, including prefix e.g. "doms:ContentModel_DOMS".
      */
     public void addContentModel(String modelPid) {
         String fragment = MODEL_TEMPLATE.replace("PID", modelPid);
         addFragmentToDescription(fragment);
-    }
-
-    private void addFragmentToDescription(String fragment) {
-        Document fragmentNode = DOM.stringToDOM(fragment, true);
-        Node importedNode = document.importNode(fragmentNode.getDocumentElement(), true);
-        rdfDescriptionNode.appendChild(importedNode);
     }
 
     /**
