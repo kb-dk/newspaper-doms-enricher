8d8cafc30fcb1f6cd0668c12634eeceb30d47443
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-08-25 10:19:07 +0200

    Review followup CR-NEWSPAPER-244: Compile pattern as constant. Better naming of variable. Better test that tests location.

--- src/main/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsLabelEnricherTreeEventHandler.java
+++ src/main/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsLabelEnricherTreeEventHandler.java
@@ -3,7 +3,7 @@ package dk.statsbiblioteket.newspaper.domsenricher.component;
 import dk.statsbibliokeket.newspaper.treenode.TreeNodeStateWithChildren;
 import dk.statsbiblioteket.doms.central.connectors.EnhancedFedora;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.NodeBeginsParsingEvent;
-import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.NodeEndParsingEvent;
+import dk.statsbiblioteket.newspaper.domsenricher.component.enrichers.NodeEnricher;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,15 +13,16 @@ import java.util.regex.Pattern;
  * Attaches somewhat descriptive labels to different types of nodes.
  */
 public class DomsLabelEnricherTreeEventHandler extends TreeNodeStateWithChildren {
+    private static final String COMMENT = NodeEnricher.COMMENT;
     private EnhancedFedora fedora;
-    private String previousAvisID;
+    private String avisID;
     private String previousBrikName;
     private String previousPageName;
-
+    private static final Pattern AVIS_PATTERN = Pattern.compile("(.*)-[0-9]*-[0-9]*$");
 
     public DomsLabelEnricherTreeEventHandler(EnhancedFedora fedora) {
         this.fedora = fedora;
-        previousAvisID = "";
+        avisID = "";
     }
 
 
@@ -39,56 +40,55 @@ public class DomsLabelEnricherTreeEventHandler extends TreeNodeStateWithChildren
         try {
             switch (getCurrentNode().getType()) {
                 case BATCH:
-                    fedora.modifyObjectLabel(currentNodePid, "batch-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "batch-" + nameOfNode, COMMENT);
                     break;
                 case WORKSHIFT_ISO_TARGET:
-                    fedora.modifyObjectLabel(currentNodePid, "workshift-iso-target", "");
+                    fedora.modifyObjectLabel(currentNodePid, "workshift-iso-target", COMMENT);
                     break;
                 case WORKSHIFT_TARGET:
-                    fedora.modifyObjectLabel(currentNodePid, "workshift-target-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "workshift-target-" + nameOfNode, COMMENT);
                     break;
                 case TARGET_IMAGE:
-                    fedora.modifyObjectLabel(currentNodePid, nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, nameOfNode, COMMENT);
                     break;
                 case FILM:
-                    fedora.modifyObjectLabel(currentNodePid, "film-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "film-" + nameOfNode, COMMENT);
                     // Put aside AvisID for use later
-                    Pattern p = Pattern.compile("(.*)-[0-9]*-[0-9]*$");
-                    Matcher m = p.matcher(nameOfNode);
+                    Matcher m = AVIS_PATTERN.matcher(nameOfNode);
                     if (m.find()) {
-                        previousAvisID = m.group(1);
+                        avisID = m.group(1);
                     } else {
-                        previousAvisID = "";
+                        avisID = "";
                     }
                     break;
                 case FILM_ISO_TARGET:
-                    fedora.modifyObjectLabel(currentNodePid, "film-iso-target", "");
+                    fedora.modifyObjectLabel(currentNodePid, "film-iso-target", COMMENT);
                     break;
                 case FILM_TARGET:
-                    fedora.modifyObjectLabel(currentNodePid, "film-target-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "film-target-" + nameOfNode, COMMENT);
                     break;
                 case ISO_TARGET_IMAGE:
-                    fedora.modifyObjectLabel(currentNodePid, "iso-target-image-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "iso-target-image-" + nameOfNode, COMMENT);
                     break;
                 case UNMATCHED:
-                    fedora.modifyObjectLabel(currentNodePid, "unmatched", "");
+                    fedora.modifyObjectLabel(currentNodePid, "unmatched", COMMENT);
                     break;
                 case EDITION:
-                    fedora.modifyObjectLabel(currentNodePid, "edition-" + previousAvisID + "-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "edition-" + avisID + "-" + nameOfNode, COMMENT);
                     break;
                 case PAGE:
-                    fedora.modifyObjectLabel(currentNodePid, "page-" + nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, "page-" + nameOfNode, COMMENT);
                     previousPageName = nameOfNode;
                     break;
                 case BRIK:
-                    fedora.modifyObjectLabel(currentNodePid, nameOfNode, "");
+                    fedora.modifyObjectLabel(currentNodePid, nameOfNode, COMMENT);
                     previousBrikName = nameOfNode;
                     break;
                 case BRIK_IMAGE:
-                    fedora.modifyObjectLabel(currentNodePid, "brik-image-" + previousBrikName, "");
+                    fedora.modifyObjectLabel(currentNodePid, "brik-image-" + previousBrikName, COMMENT);
                     break;
                 case PAGE_IMAGE:
-                    fedora.modifyObjectLabel(currentNodePid, "page-image-" + previousPageName, "");
+                    fedora.modifyObjectLabel(currentNodePid, "page-image-" + previousPageName, COMMENT);
                     break;
             }
         } catch (Exception e) {
--- src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsLabelEnricherTreeEventHandlerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsLabelEnricherTreeEventHandlerTest.java
@@ -8,6 +8,7 @@ import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.TreeIterator
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.DefaultTreeEventHandler;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.EventRunner;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.TreeEventHandler;
+import dk.statsbiblioteket.newspaper.domsenricher.component.enrichers.NodeEnricher;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -44,63 +45,63 @@ public class DomsLabelEnricherTreeEventHandlerTest {
                                             true, true, true, true, true, true, true, true, true, true, true, true,
                                             true, true, true, true, true, true, true, true, true, true, true, true,
                                             false);
-        when(iterator.next()).thenReturn(new NodeBeginsParsingEvent("B4004-RT1"),
-                                         new NodeBeginsParsingEvent("newspaper-4004-1"),
-                                         new NodeBeginsParsingEvent("1970-01-01-01"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001A"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001A.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001A.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001A"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001B"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001B.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001B.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001B"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001-brik"),
-                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001-brik.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001-brik.jp2"),
-                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001-brik"),
-                                         new NodeEndParsingEvent("1970-01-01-01"),
-                                         new NodeBeginsParsingEvent("UNMATCHED"),
-                                         new NodeBeginsParsingEvent("newspaper-4004-1-0002"),
-                                         new NodeBeginsParsingEvent("newspaper-4004-1-0002.jp2"),
-                                         new NodeEndParsingEvent("newspaper-4004-1-0002.jp2"),
-                                         new NodeEndParsingEvent("newspaper-4004-1-0002"),
-                                         new NodeEndParsingEvent("UNMATCHED"),
-                                         new NodeBeginsParsingEvent("FILM-ISO-target"),
-                                         new NodeBeginsParsingEvent("newspaper-4004-1-0003"),
-                                         new NodeBeginsParsingEvent("newspaper-4004-1-0003.jp2"),
-                                         new NodeEndParsingEvent("newspaper-4004-1-0003.jp2"),
-                                         new NodeEndParsingEvent("newspaper-4004-1-0003"),
-                                         new NodeEndParsingEvent("FILM-ISO-target"),
-                                         new NodeEndParsingEvent("avisid-4004-1"),
-                                         new NodeBeginsParsingEvent("WORKSHIFT-ISO-TARGET"),
-                                         new NodeBeginsParsingEvent("Target-000001-0001"),
-                                         new NodeBeginsParsingEvent("Target-000001-0001.jp2"),
-                                         new NodeEndParsingEvent("Target-000001-0001.jp2"),
-                                         new NodeEndParsingEvent("Target-000001-0001"),
-                                         new NodeEndParsingEvent("WORKSHIFT-ISO-TARGET"),
-                                         new NodeEndParsingEvent("B4004-RT1"),
+        when(iterator.next()).thenReturn(new NodeBeginsParsingEvent("B4004-RT1", "uuid:00000000-0000-0000-0000-000000000001"),
+                                         new NodeBeginsParsingEvent("newspaper-4004-1", "uuid:00000000-0000-0000-0000-000000000002"),
+                                         new NodeBeginsParsingEvent("1970-01-01-01", "uuid:00000000-0000-0000-0000-000000000003"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001A", "uuid:00000000-0000-0000-0000-000000000004"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001A.jp2", "uuid:00000000-0000-0000-0000-000000000005"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001A.jp2", "uuid:00000000-0000-0000-0000-000000000005"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001A", "uuid:00000000-0000-0000-0000-000000000004"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001B", "uuid:00000000-0000-0000-0000-000000000006"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001B.jp2", "uuid:00000000-0000-0000-0000-000000000007"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001B.jp2", "uuid:00000000-0000-0000-0000-000000000007"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001B", "uuid:00000000-0000-0000-0000-000000000006"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001-brik", "uuid:00000000-0000-0000-0000-000000000008"),
+                                         new NodeBeginsParsingEvent("newspaper-1970-01-01-01-0001-brik.jp2", "uuid:00000000-0000-0000-0000-000000000009"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001-brik.jp2", "uuid:00000000-0000-0000-0000-000000000009"),
+                                         new NodeEndParsingEvent("newspaper-1970-01-01-01-0001-brik", "uuid:00000000-0000-0000-0000-000000000008"),
+                                         new NodeEndParsingEvent("1970-01-01-01", "uuid:00000000-0000-0000-0000-000000000003"),
+                                         new NodeBeginsParsingEvent("UNMATCHED", "uuid:00000000-0000-0000-0000-000000000010"),
+                                         new NodeBeginsParsingEvent("newspaper-4004-1-0002", "uuid:00000000-0000-0000-0000-000000000011"),
+                                         new NodeBeginsParsingEvent("newspaper-4004-1-0002.jp2", "uuid:00000000-0000-0000-0000-000000000012"),
+                                         new NodeEndParsingEvent("newspaper-4004-1-0002.jp2", "uuid:00000000-0000-0000-0000-000000000012"),
+                                         new NodeEndParsingEvent("newspaper-4004-1-0002", "uuid:00000000-0000-0000-0000-000000000011"),
+                                         new NodeEndParsingEvent("UNMATCHED", "uuid:00000000-0000-0000-0000-000000000010"),
+                                         new NodeBeginsParsingEvent("FILM-ISO-target", "uuid:00000000-0000-0000-0000-000000000013"),
+                                         new NodeBeginsParsingEvent("newspaper-4004-1-0003", "uuid:00000000-0000-0000-0000-000000000014"),
+                                         new NodeBeginsParsingEvent("newspaper-4004-1-0003.jp2", "uuid:00000000-0000-0000-0000-000000000015"),
+                                         new NodeEndParsingEvent("newspaper-4004-1-0003.jp2", "uuid:00000000-0000-0000-0000-000000000015"),
+                                         new NodeEndParsingEvent("newspaper-4004-1-0003", "uuid:00000000-0000-0000-0000-000000000014"),
+                                         new NodeEndParsingEvent("FILM-ISO-target", "uuid:00000000-0000-0000-0000-000000000013"),
+                                         new NodeEndParsingEvent("newspaper-4004-1", "uuid:00000000-0000-0000-0000-000000000002"),
+                                         new NodeBeginsParsingEvent("WORKSHIFT-ISO-TARGET", "uuid:00000000-0000-0000-0000-000000000016"),
+                                         new NodeBeginsParsingEvent("Target-000001-0001", "uuid:00000000-0000-0000-0000-000000000017"),
+                                         new NodeBeginsParsingEvent("Target-000001-0001.jp2", "uuid:00000000-0000-0000-0000-000000000018"),
+                                         new NodeEndParsingEvent("Target-000001-0001.jp2", "uuid:00000000-0000-0000-0000-000000000018"),
+                                         new NodeEndParsingEvent("Target-000001-0001", "uuid:00000000-0000-0000-0000-000000000017"),
+                                         new NodeEndParsingEvent("WORKSHIFT-ISO-TARGET", "uuid:00000000-0000-0000-0000-000000000016"),
+                                         new NodeEndParsingEvent("B4004-RT1", "uuid:00000000-0000-0000-0000-000000000001"),
                                          null);
         TreeEventHandler labelHandler = new DomsLabelEnricherTreeEventHandler(fedora);
         new EventRunner(iterator, Arrays.asList(labelHandler), resultCollector).run();
-        verify(fedora).modifyObjectLabel(anyString(), eq("batch-B4004-RT1"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("film-newspaper-4004-1"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("edition-newspaper-1970-01-01-01"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-newspaper-1970-01-01-01-0001A"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-image-newspaper-1970-01-01-01-0001A"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-newspaper-1970-01-01-01-0001B"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-image-newspaper-1970-01-01-01-0001B"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("newspaper-1970-01-01-01-0001-brik"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("brik-image-newspaper-1970-01-01-01-0001-brik"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("unmatched"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-newspaper-4004-1-0002"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("page-image-newspaper-4004-1-0002"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("film-iso-target"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("film-target-newspaper-4004-1-0003"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("iso-target-image-newspaper-4004-1-0003.jp2"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("workshift-iso-target"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("workshift-target-Target-000001-0001"), anyString());
-        verify(fedora).modifyObjectLabel(anyString(), eq("Target-000001-0001.jp2"), anyString());
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000001"), eq("batch-B4004-RT1"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000002"), eq("film-newspaper-4004-1"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000003"), eq("edition-newspaper-1970-01-01-01"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000004"), eq("page-newspaper-1970-01-01-01-0001A"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000005"), eq("page-image-newspaper-1970-01-01-01-0001A"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000006"), eq("page-newspaper-1970-01-01-01-0001B"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000007"), eq("page-image-newspaper-1970-01-01-01-0001B"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000008"), eq("newspaper-1970-01-01-01-0001-brik"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000009"), eq("brik-image-newspaper-1970-01-01-01-0001-brik"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000010"), eq("unmatched"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000011"), eq("page-newspaper-4004-1-0002"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000012"), eq("page-image-newspaper-4004-1-0002"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000013"), eq("film-iso-target"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000014"), eq("film-target-newspaper-4004-1-0003"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000015"), eq("iso-target-image-newspaper-4004-1-0003.jp2"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000016"), eq("workshift-iso-target"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000017"), eq("workshift-target-Target-000001-0001"), eq(NodeEnricher.COMMENT));
+        verify(fedora).modifyObjectLabel(eq("uuid:00000000-0000-0000-0000-000000000018"), eq("Target-000001-0001.jp2"), eq(NodeEnricher.COMMENT));
         verifyNoMoreInteractions(fedora);
         assertTrue(resultCollector.isSuccess());
     }
