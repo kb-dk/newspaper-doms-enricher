50d8e49cfa93866ab19ffad19619f34d6c948242
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2014-09-16 13:15:24 +0200

    Review Followup of CR-NEWSPAPER-252 No declaraton of java.lang and 1 extra unit test.

--- src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsPublisherEventHandlerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsPublisherEventHandlerTest.java
@@ -100,4 +100,36 @@ public class DomsPublisherEventHandlerTest {
                                                "Could not publish doms object: " + exception.toString());
         verifyNoMoreInteractions(resultCollectorMock);
     }
+
+    /**
+     * Exceptional case: publishing throws exceptions, and tries are set to 1. Fails after 1 call, and result is
+     * collected.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testPublishingWithNoRetries() throws Exception {
+        EnhancedFedora enhancedFedoraMock = mock(EnhancedFedora.class);
+        BackendMethodFailedException exception = new BackendMethodFailedException("exceptionMessage");
+        doThrow(exception).when(enhancedFedoraMock)
+                .modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
+        int tries = 1;
+        ResultCollector resultCollectorMock = mock(ResultCollector.class);
+        DomsPublisherEventHandler domsPublisherEventHandler = new DomsPublisherEventHandler(enhancedFedoraMock,
+                                                                                            resultCollectorMock, 1,
+                                                                                            tries);
+        domsPublisherEventHandler.handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1", "uuid:1"));
+        domsPublisherEventHandler
+                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1", "uuid:2"));
+        domsPublisherEventHandler
+                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1/1970-01", "uuid:3"));
+        domsPublisherEventHandler.handleFinish();
+        verify(enhancedFedoraMock, times(tries)).modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
+        verify(enhancedFedoraMock).modifyObjectState("uuid:3", "A", NodeEnricher.COMMENT);
+        verifyNoMoreInteractions(enhancedFedoraMock);
+        verify(resultCollectorMock).addFailure("B000000000000-RT1/000000000000-1", "metadata",
+                                               DomsPublisherEventHandler.class.getSimpleName(),
+                                               "Could not publish doms object: " + exception.toString());
+        verifyNoMoreInteractions(resultCollectorMock);
+    }
 }
