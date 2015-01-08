bac1fd3552b33eac0d605468177e6038a1022ba0
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-01-08 15:52:11 +0100

    Remove tests that test retrying

--- src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsPublisherEventHandlerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/domsenricher/component/DomsPublisherEventHandlerTest.java
@@ -43,65 +43,7 @@ public class DomsPublisherEventHandlerTest {
     }
 
     /**
-     * Exceptional case: publishing throws exception once and, and is retried then succeeds.
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testPublishingWithOneRetry() throws Exception {
-        EnhancedFedora enhancedFedoraMock = mock(EnhancedFedora.class);
-        doThrow(new BackendMethodFailedException("")).doNothing().when(enhancedFedoraMock)
-                .modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
-        int tries = 3;
-        ResultCollector resultCollectorMock = mock(ResultCollector.class);
-        DomsPublisherEventHandler domsPublisherEventHandler = new DomsPublisherEventHandler(enhancedFedoraMock,
-                                                                                            resultCollectorMock, 1);
-        domsPublisherEventHandler.handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1", "uuid:1"));
-        domsPublisherEventHandler
-                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1", "uuid:2"));
-        domsPublisherEventHandler
-                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1/1970-01", "uuid:3"));
-        domsPublisherEventHandler.handleFinish();
-        verify(enhancedFedoraMock, times(2)).modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
-        verify(enhancedFedoraMock).modifyObjectState("uuid:3", "A", NodeEnricher.COMMENT);
-        verifyNoMoreInteractions(enhancedFedoraMock);
-        verifyNoMoreInteractions(resultCollectorMock);
-    }
-
-    /**
-     * Exceptional case: publishing throws exceptions, and is retried the expected number of times, and result is
-     * collected.
-     *
-     * @throws Exception
-     */
-    @Test
-    public void testPublishingWithMaxRetries() throws Exception {
-        EnhancedFedora enhancedFedoraMock = mock(EnhancedFedora.class);
-        BackendMethodFailedException exception = new BackendMethodFailedException("exceptionMessage");
-        doThrow(exception).when(enhancedFedoraMock)
-                .modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
-        int tries = 3;
-        ResultCollector resultCollectorMock = mock(ResultCollector.class);
-        DomsPublisherEventHandler domsPublisherEventHandler = new DomsPublisherEventHandler(enhancedFedoraMock,
-                                                                                            resultCollectorMock, 1);
-        domsPublisherEventHandler.handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1", "uuid:1"));
-        domsPublisherEventHandler
-                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1", "uuid:2"));
-        domsPublisherEventHandler
-                .handleNodeBegin(new NodeBeginsParsingEvent("B000000000000-RT1/000000000000-1/1970-01", "uuid:3"));
-        domsPublisherEventHandler.handleFinish();
-        verify(enhancedFedoraMock, times(tries)).modifyObjectState("uuid:2", "A", NodeEnricher.COMMENT);
-        verify(enhancedFedoraMock).modifyObjectState("uuid:3", "A", NodeEnricher.COMMENT);
-        verifyNoMoreInteractions(enhancedFedoraMock);
-        verify(resultCollectorMock).addFailure("B000000000000-RT1/000000000000-1", "metadata",
-                                               DomsPublisherEventHandler.class.getSimpleName(),
-                                               "Could not publish doms object: " + exception.toString());
-        verifyNoMoreInteractions(resultCollectorMock);
-    }
-
-    /**
-     * Exceptional case: publishing throws exceptions, and tries are set to 1. Fails after 1 call, and result is
-     * collected.
+     * Exceptional case: publishing throws exceptions. Fails after that call, and result is collected.
      *
      * @throws Exception
      */
